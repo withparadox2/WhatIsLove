a565eeb265dabf4c701496ac017e3830b20b325a
withparadox2 <withparadox2@gmail.com>
2013-07-17 18:13:35 +0800

    add keyevent

--- ex.js
+++ ex.js
@@ -12,24 +12,87 @@ var canvas = document.getElementById('canvas'),
     cw = 525,
     ch = 525,
     xPos = 0,
-    yPos = 0;
+    yPos = 0,
+	xVel = 0,
+	yVel = 0,
+	xAcc = 0,
+	yAcc = 0,
+	cwCenter = cw / 2,
+	chCenter = ch / 2,
+	radius = 3;
 
 canvas.width = cw;
 canvas.height = ch;
 
 function drawShape(x, y){
-    ctx.fillStyle='#FF0000';
-    ctx.fillRect(x, y, 20, 30);
+	ctx.fillStyle="#FFF";
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, Math.PI*2, true);
+	ctx.arc(cwCenter, chCenter, radius, 0, Math.PI*2, true);
+	ctx.closePath();	
+	ctx.fill();
 }
 
 function loop() {
     requestAnimFrame( loop );
     ctx.clearRect(0, 0, 525, 525);
+	calculatePos();
     drawShape(xPos, yPos);
-    xPos = xPos + 2;
-    yPos = 100 * (1-Math.sin(xPos/50));
-    if(xPos > 525) xPos = 0;
-    if(yPos > 525) yPos = 0;
+}
+
+function calculatePos(){
+	yVel = yVel + yAcc;
+	yPos = yPos + yVel;
+	if(yPos > 525) yPos = 0;
+	if(yPos < 0  ) yPos = 525;
+	if(Math.abs(yVel) < 0.1){
+	   	yAcc = 0;
+		yVel = 0;
+	}
+	xVel = xVel + xAcc;
+	xPos = xPos + xVel;
+	if(xPos > 525) xPos = 0;
+	if(xPos < 0  ) xPos = 525;
+	if(Math.abs(xVel) < 0.1){
+	   	xAcc = 0;
+		xVel = 0;
+	}
+}
+
+function doKeyDown(evt){
+	switch(evt.keyCode){
+		case 38: //up arrow
+			yAcc = -0.1;
+			break;
+		case 40: //down arrow
+			yAcc = 0.1;
+			break;
+		case 37: //left arrow
+			xAcc = -0.1;
+			break;
+		case 39: //down arrow
+			xAcc = 0.1;
+			break;
+	}
+}
+
+function doKeyUp(evt){
+	switch(evt.keyCode){
+		case 38: //up arrow
+			yAcc = (yVel < 0) ? 0.1 : -0.1;
+			break;
+		case 40: //down arrow
+			yAcc = (yVel < 0) ? 0.1 : -0.1;
+			break;
+		case 37: //left arrow
+			xAcc = (xVel < 0) ? 0.1 : -0.1;
+			break;
+		case 39: //down arrow
+			xAcc = (xVel < 0) ? 0.1 : -0.1;
+			break;
+	}
 }
 
 window.onload=loop;
+window.addEventListener('keydown', doKeyDown, true);
+window.addEventListener('keyup', doKeyUp, true);
