e4b95471e1c580ce3413365e9cfa5f664e70646c
withparadox2 <withparadox2@gmail.com>
2013-07-17 12:39:46 +0800

    nothing

--- ex.js
+++ ex.js
@@ -1,33 +1,35 @@
 window.requestAnimFrame = ( function() {
-	return window.requestAnimationFrame ||
-				window.webkitRequestAnimationFrame ||
-				window.mozRequestAnimationFrame ||
-				function( callback ) {
-					window.setTimeout( callback, 1000 / 60 );
-				};
+    return window.requestAnimationFrame ||
+	window.webkitRequestAnimationFrame ||
+	window.mozRequestAnimationFrame ||
+	function( callback ) {
+	    window.setTimeout( callback, 1000 / 60 );
+	};
 })();
 
 var canvas = document.getElementById('canvas'),
-	ctx = canvas.getContext('2d'),
-	xPos = 0,
-	yPos = 0;
+    ctx = canvas.getContext('2d'),
+    cw = 525,
+    ch = 525,
+    xPos = 0,
+    yPos = 0;
 
+canvas.width = cw;
+canvas.height = ch;
 
 function drawShape(x, y){
-	ctx.fillStyle='#FF0000';
-	ctx.fillRect(x, y, 20, 10);
+    ctx.fillStyle='#FF0000';
+    ctx.fillRect(x, y, 20, 30);
 }
-
-ctx.fillRect(20,20,40,40);
 
 function loop() {
-	requestAnimFrame( loop );
-	ctx.clearRect(0, 0, 525, 525);
-	drawShape(xPos, yPos);
-	xPos = xPos + 2;
-	yPos = yPos + 2;
-	if(xPos > 525) xPos = 0;
-	if(yPos > 525) yPos = 0;
+    requestAnimFrame( loop );
+    ctx.clearRect(0, 0, 525, 525);
+    drawShape(xPos, yPos);
+    xPos = xPos + 2;
+    yPos = 100 * (1-Math.sin(xPos/50));
+    if(xPos > 525) xPos = 0;
+    if(yPos > 525) yPos = 0;
 }
 
-//window.onload=loop;
+window.onload=loop;
